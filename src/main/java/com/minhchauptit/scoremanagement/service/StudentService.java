262bab9140e6661293d844121347c48be5a58d7a
Minh Chau <minhchauptit@outlook.com>
2020-02-11 22:57:56 +0700

    create service layer

---
+++ src/main/java/com/minhchauptit/scoremanagement/service/StudentService.java
@@ -0,0 +1,12 @@
+package com.minhchauptit.scoremanagement.service;
+
+import com.minhchauptit.scoremanagement.entity.Student;
+
+import java.util.List;
+
+public interface StudentService {
+    List<Student> findAll();
+    Student findById(Integer studentId);
+    Student save(Student student);
+    void delete(Integer studentId);
+}
---
+++ src/main/java/com/minhchauptit/scoremanagement/serviceimpl/StudentServiceImpl.java
@@ -0,0 +1,40 @@
+package com.minhchauptit.scoremanagement.serviceimpl;
+
+import com.minhchauptit.scoremanagement.entity.Student;
+import com.minhchauptit.scoremanagement.repository.StudentRespository;
+import com.minhchauptit.scoremanagement.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+
+    @Autowired
+    private StudentRespository studentRespository;
+
+    @Override
+    public List<Student> findAll() {
+        return studentRespository.findAll();
+    }
+
+    @Override
+    public Student findById(Integer studentId) {
+        Optional<Student> optionalStudent =  studentRespository.findById(studentId);
+        if(optionalStudent.isPresent()) return optionalStudent.get();
+        return null;
+    }
+
+    @Override
+    public Student save(Student student) {
+        return studentRespository.save(student);
+
+    }
+
+    @Override
+    public void delete(Integer studentId) {
+        studentRespository.deleteById(studentId);
+    }
+}
