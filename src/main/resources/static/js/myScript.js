6e9da8646bfc225824b482c54bea5594ff31c599
Minh Chau <minhchauptit@outlook.com>
2020-02-14 00:06:54 +0700

    add subject pages

--- src/main/resources/static/js/myScript.js
+++ src/main/resources/static/js/myScript.js
@@ -1,3 +1,8 @@
+function activeParentMenu(id,subId) {
+    $('#'+id).prop("class","active");
+    $('#'+subId).prop("class","collapse in")
+}
+
 function activeMenu(id) {
     $('#'+id).prop("class","active");
 }
