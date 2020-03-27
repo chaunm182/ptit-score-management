8404fe2b3e1183901ec216728f6b2e678c8a9fa1
Minh Chau <minhchauptit@outlook.com>
2020-03-27 20:10:52 +0700

    change something

--- src/main/java/com/minhchauptit/scoremanagement/config/AspectConfiguration.java
+++ src/main/java/com/minhchauptit/scoremanagement/config/AspectConfiguration.java
@@ -1,6 +1,5 @@
 package com.minhchauptit.scoremanagement.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
