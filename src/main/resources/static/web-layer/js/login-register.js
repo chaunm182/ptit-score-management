cbacc81c31c8e068063f0d9f6c0769729e9b0b6f
Minh Chau <minhchauptit@outlook.com>
2020-04-07 09:38:01 +0700

    change something

--- src/main/resources/static/web-layer/js/login-register.js
+++ src/main/resources/static/web-layer/js/login-register.js
@@ -47,35 +47,52 @@ function shakeModal(){
 var formLogin = $('#form-login');
 formLogin.on('submit',function (e) {
     e.preventDefault();
+    // var form = new FormData();
+    // var btnSubmit = $('#btnSubmitLogin');
+    // form.append("username", $('#username').val());
+    // form.append("password", $('#password').val());
+    //
+    // $.ajax({
+    //     url:  formLogin.attr('action'),
+    //     method: "POST",
+    //     processData: false,
+    //     contentType: false,
+    //     data: form,
+    //     timeout: 0,
+    //     beforeSend: function(){
+    //         btnSubmit.prop('disabled',true);
+    //         btnSubmit.val('Vui lòng chờ...');
+    //     },
+    //     success: function (res) {
+    //         console.log(res);
+    //         if(res.trim()==='false'){
+    //             btnSubmit.prop('disabled',false);
+    //             btnSubmit.val('Đăng nhập');
+    //             shakeModal();
+    //         }
+    //         else{
+    //             $(location).attr('href','/');
+    //         }
+    //     },
+    //     error: function (res) {
+    //         console.log(res);
+    //     }
+    // });
     var form = new FormData();
-    var btnSubmit = $('#btnSubmitLogin');
     form.append("username", $('#username').val());
     form.append("password", $('#password').val());
 
-    $.ajax({
-        url:  formLogin.attr('action'),
-        method: "POST",
-        processData: false,
-        contentType: false,
-        data: form,
-        timeout: 0,
-        beforeSend: function(){
-            btnSubmit.prop('disabled',true);
-            btnSubmit.val('Vui lòng chờ...');
-        },
-        success: function (res) {
-            console.log(res);
-            if(res.trim()==='false'){
-                btnSubmit.prop('disabled',false);
-                btnSubmit.val('Đăng nhập');
-                shakeModal();
-            }
-            else{
-                $(location).attr('href','/');
-            }
-        },
-        error: function (res) {
-            console.log(res);
-        }
+    var settings = {
+        "url": formLogin.attr('action'),
+        "method": "POST",
+        "timeout": 0,
+        "processData": false,
+        "mimeType": "multipart/form-data",
+        "contentType": false,
+        "data": form
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
     });
 });
